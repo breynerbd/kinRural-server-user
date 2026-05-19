92822116dbc55ae1c610d5d1641b090c30e076a8
AndyAjiatas07 <aajiatas-2021496@kinal.edu.gt>
2026-05-19 11:57:15 -0600

    T37 - implementación parcial de mejor register

--- src/utils/getInternalUser.js
+++ src/utils/getInternalUser.js
@@ -1,27 +1,13 @@
 import { User } from "../users/user.model.js";
 
-export const getInternalUser = async (authId, email) => {
+export const getInternalUser = async (authId) => {
     if (!authId) {
         throw new Error("authId no puede ser undefined");
     }
 
-    let user = await User.findOne({
+    const user = await User.findOne({
         where: { auth_id: authId }
     });
-
-    if (!user) {
-        user = await User.create({
-            auth_id: authId,
-            nombre: "Pendiente",
-            apellido: "Pendiente",
-            correo: email,
-            dpi: null,
-            telefono: "PENDIENTE",
-            direccion: "PENDIENTE",
-            ingresos_mensuales: 0,
-            role_id: 2
-        });
-    }
 
     return user;
 };
